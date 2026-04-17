473e49f96090812fd0c370bc9139c4a5d997d23e
Efrat <efrat@cadysolutions.com>
2026-04-17 11:33:25 +0300

    add pipeline_message.py: PipelineMessage IPC data class

---
+++ pipeline_message.py
@@ -0,0 +1,10 @@
+class PipelineMessage:
+    def __init__(self, frame, frame_index, detections, is_sentinel):
+        self.frame = frame
+        self.frameIndex = frame_index
+        self.detections = detections
+        self.isSentinel = is_sentinel
+
+    @classmethod
+    def create_sentinel(cls):
+        return cls(None, None, None, True)
