96b4fdeb9af405ece35695c0c78b818966962c3d
Efrat <efrat@cadysolutions.com>
2026-04-17 12:22:55 +0300

    blur detected regions in displayer (Step B). split each separate drawing step to its own function

--- displayer.py
+++ displayer.py
@@ -9,6 +9,7 @@ BOX_COLOR = (0, 255, 0)
 BOX_THICKNESS = 2
 TEXT_COLOR = (255, 255, 255)
 TEXT_POSITION = (10, 30)
+BLUR_KERNEL_SIZE = (51, 51)
 
 
 class Displayer:
@@ -16,12 +17,29 @@ class Displayer:
         self._inputQueue = input_queue
 
     @staticmethod
-    def _draw(display_frame, detections):
+    def _blur_detections(display_frame, detections):
+        for (x, y, w, h) in detections:
+            display_frame[y:y + h, x:x + w] = cv2.GaussianBlur(
+                display_frame[y:y + h, x:x + w],
+                BLUR_KERNEL_SIZE,
+                0
+            )
+
+    @staticmethod
+    def _draw_boxes(display_frame, detections):
         for (x, y, w, h) in detections:
             cv2.rectangle(display_frame, (x, y), (x + w, y + h), BOX_COLOR, BOX_THICKNESS)
+
+    @staticmethod
+    def _draw_timestamp(display_frame):
         current_time = datetime.now().strftime("%H:%M:%S")
         cv2.putText(display_frame, current_time, TEXT_POSITION, FONT, FONT_SCALE, TEXT_COLOR, FONT_THICKNESS)
 
+    @staticmethod
+    def _draw(display_frame, detections):
+        Displayer._blur_detections(display_frame, detections)
+        Displayer._draw_boxes(display_frame, detections)
+        Displayer._draw_timestamp(display_frame)
     def run(self):
         while True:
             msg = self._inputQueue.get()
